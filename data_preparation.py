392105f9d9d692eb88003e95afa8f8e889eda8ca
AtrCheema <ather_abbas786@yahoo.com>
2020-04-22 10:17:46 +0900

    pep8 compatible

--- data_preparation.py
+++ data_preparation.py
@@ -7,6 +7,7 @@ from utils import do_plot
 
 # TODO all events must start with some data prior to observations
 
+
 class DATA(object):
     """
     `attributes`
@@ -33,19 +34,20 @@ class DATA(object):
         self.df = self.get_df()
         self.set_attrs()
 
-
     def get_df(self):
         # 20180601 - 201909-30, (5856,3)
         f = os.path.join(self.data_dir, 'wat_data_30min.txt')
         wat_df = pd.read_csv(f)
         wat_df.index = pd.to_datetime(wat_df['Date_Time'])
-        if 'Date_Time' in wat_df.columns: wat_df.pop('Date_Time')
+        if 'Date_Time' in wat_df.columns:
+            wat_df.pop('Date_Time')
 
         # 201806 - 20190906,  (1176, 8)
         f = os.path.join(self.data_dir, 'env_data_30min.txt')
         env_df = pd.read_csv(f)
         env_df.index = pd.to_datetime(env_df['Date_Time'])
-        if 'Date_Time' in env_df.columns: env_df.pop('Date_Time')
+        if 'Date_Time' in env_df.columns:
+            env_df.pop('Date_Time')
 
         # (295, 12)
         obs_df = self.load_obs_data(sheets=['201806', '201905', '201908_1', '201908_2'])
@@ -71,12 +73,11 @@ class DATA(object):
 
         return
 
-
-
     def plot_data(self):
-        obs_logy=False
+        obs_logy = False
         for out in self.all_outs:
-            if out in self.args: obs_logy=True
+            if out in self.args:
+                obs_logy = True
             idx = 0
             for in_type in [self.rains, self.misc_in, self.env]:
                 plt_df = pd.DataFrame(index=self.df.index)
@@ -86,26 +87,22 @@ class DATA(object):
 
                 plt_df = remove_chunk('20180630', '20190516', plt_df)
 
-                do_plot(plt_df, plt_df.columns, save_name= 'results/' +out + '_' + str(idx), obs_logy=obs_logy)
-                idx +=1
+                do_plot(plt_df, plt_df.columns, save_name='results/' + out + '_' + str(idx), obs_logy=obs_logy)
+                idx += 1
         return
 
-
-
-    def load_obs_data(self, desired_output = None, sheets=None):
+    def load_obs_data(self, desired_output=None, sheets=None):
         """ gwangali site data at 1 hour frequency, contains all data, input and output ts"""
         if desired_output is None:
-            desired_output = ['ecoli', '16s', 'inti1', 'Total_args',
-                   'tetx_coppml', 'sul1_coppml', 'blaTEM_coppml', 'aac_coppml', 'Total_otus', 'otu_5575',
-                      'otu_273', 'otu_94']
+            desired_output = ['ecoli', '16s', 'inti1', 'Total_args', 'tetx_coppml', 'sul1_coppml', 'blaTEM_coppml',
+                              'aac_coppml', 'Total_otus', 'otu_5575', 'otu_273', 'otu_94']
         columns = ['pcp1', 'pcp3', 'pcp6', 'pcp12', 'tide', 'W_temp', 'sal', 'wind_sp',
                    'wind_dir', 'atm_temp', 'atm_p', 'mslp_hpa', 'rel_hum',  'ecoli', '16s', 'inti1', 'Total_args',
                    'tetx_coppml', 'sul1_coppml', 'blaTEM_coppml', 'aac_coppml', 'Total_otus', 'otu_5575',
-                      'otu_273', 'otu_94']
+                   'otu_273', 'otu_94']
 
         if sheets is None:
-            sheets = ['201806', '201905', '201908_1', '201908_2'#, '201909'
-                  ]
+            sheets = ['201806', '201905', '201908_1', '201908_2', '201909']
 
         f = os.path.join(self.data_dir, 'Time_series_data.xlsx')
         haupt_df = pd.DataFrame()
@@ -123,16 +120,19 @@ class DATA(object):
                 dayfirst = True
             idx = pd.to_datetime(idx1, yearfirst=yearfirst, dayfirst=dayfirst)
             df.index = idx
-            if not isinstance(df.index, pd.DatetimeIndex): raise TypeError
+            if not isinstance(df.index, pd.DatetimeIndex):
+                raise TypeError
             df.index.freq = pd.infer_freq(df.index)
-            if 'date' in df.columns: df.pop('date')
-            if 'time' in df.columns: df.pop('time')
-            if 'Sample No.' in df.columns: df.pop('Sample No.')
+            if 'date' in df.columns:
+                df.pop('date')
+            if 'time' in df.columns:
+                df.pop('time')
+            if 'Sample No.' in df.columns:
+                df.pop('Sample No.')
             df.columns = columns
 
             haupt_df = pd.concat([haupt_df, df])
         return haupt_df[desired_output]
-
 
     def load_wat_data(self, desired_output=None):
         """1 minute data at a site 10 km away from Gwangali"""
@@ -153,21 +153,23 @@ class DATA(object):
                 idx = pd.to_datetime(_df['Date_Time'])
                 _df.index = idx
                 _df.index.freq = pd.infer_freq(_df.index)
-                if not isinstance(_df.index, pd.DatetimeIndex): raise ValueError
-                if not isinstance(_df.index.freqstr, str): raise ValueError
-                if self.verbosity>0: print(_f, _df.index.freq)
+                if not isinstance(_df.index, pd.DatetimeIndex):
+                    raise ValueError
+                if not isinstance(_df.index.freqstr, str):
+                    raise ValueError
+                if self.verbosity > 0:
+                    print(_f, _df.index.freq)
 
                 ts = pd.DataFrame()
-                for col in desired_output: #_df.columns:
+                for col in desired_output:  # _df.columns:
                     to_resample = pd.DataFrame(_df[col])
                     _ts = down_sample(to_resample, col, self.freq, idx=None, verbosity=self.verbosity)
-                    ts = pd.concat([ts, _ts], axis=1,sort=False)
+                    ts = pd.concat([ts, _ts], axis=1, sort=False)
 
                 haupt_df = pd.concat([haupt_df, ts])
             final_df = haupt_df[desired_output]
             final_df.to_excel(os.path.join(self.data_dir), 'wat_data_' + self.freq + 'txt')
             return final_df
-
 
     def load_env_data(self, desired_output=None):
         """ loads 1 minute data from a site located 10 km from Gwangali.
@@ -183,26 +185,27 @@ class DATA(object):
                                   'air_p_hpa', 'mslp_hpa', 'rel_hum']
             cols = ["Point_No", "Point", "Date_Time",
                     "air_temp_c", "pcp_mm", "wind_dir_deg", "wind_speed_mps",
-                    "air_p_hpa","mslp_hpa","rel_hum"]
+                    "air_p_hpa", "mslp_hpa", "rel_hum"]
 
             d_dir = os.path.join(self.data_dir, 'AWS_data')
             files = [f for f in os.listdir(d_dir) if f.endswith('txt')]
-            Haupt_df = pd.DataFrame()
+            haupt_df = pd.DataFrame()
 
             for _f in files:
                 f = os.path.join(d_dir, _f)
                 file_df = pd.read_csv(f, sep='\t')
 
-
                 col_df = pd.DataFrame()
                 for col in cols:  # for each columns in file
                     col_df1 = pd.DataFrame()
                     col_df2 = pd.DataFrame()
-                    for site in ['1', '2']:  # each file contains samples from two sites whose columns have suffix 1 and 2
+                    #  each file contains samples from two sites whose columns have suffix 1 and 2
+                    for site in ['1', '2']:
                         _idx = file_df['Date_Time' + site]
                         _col = col + site
                         _df1 = pd.DataFrame(file_df[_col])
-                        _df1 = assign_freq(_df1, _idx, _f+' ' + col, force_freq='1min', verbosity=self.verbosity-1, print_only=False)
+                        _df1 = assign_freq(_df1, _idx, _f+' ' + col, force_freq='1min', verbosity=self.verbosity-1,
+                                           print_only=False)
                         if site == '1':
                             col_df1 = pd.concat([col_df1, _df1], axis=1, sort=False)
                         else:
@@ -212,25 +215,30 @@ class DATA(object):
                     if nans_1 > nans_2:
                         col_df2.columns = [col]
                         col_df = pd.concat([col_df, col_df2], axis=1, sort=False)
-                        if self.verbosity>1:    print('for {}, {} is chosen which had {} nans while {} had {} nans'.format(col, 2, nans_2, 1, nans_1))
+                        if self.verbosity > 1:
+                            print('for {}, {} is chosen which had {} nans while {} had {} nans'.format(col, 2, nans_2,
+                                                                                                       1, nans_1))
                     else:
                         col_df1.columns = [col]
-                        col_df1 = assign_freq(col_df1, _idx, _f + ' ' + col, force_freq='1min', verbosity=self.verbosity-1)
+                        col_df1 = assign_freq(col_df1, _idx, _f + ' ' + col, force_freq='1min',
+                                              verbosity=self.verbosity-1)
                         col_df = pd.concat([col_df, col_df1], axis=1, sort=False)
-                        if self.verbosity>1:    print('for {}, {} is chosen which had {} nans while {} had {} nans'.format(col, 1, nans_1, 2, nans_2))
+                        if self.verbosity > 1:
+                            print('for {}, {} is chosen which had {} nans while {} had {} nans'
+                                  .format(col, 1, nans_1, 2, nans_2))
 
                     col_df = down_sample(col_df, col, self.freq, _idx, self.verbosity)
 
-                col_df = assign_freq(col_df, file = _f, force_freq=None, verbosity=self.verbosity, print_only=True)
+                col_df = assign_freq(col_df, file=_f, force_freq=None, verbosity=self.verbosity, print_only=True)
 
-                Haupt_df = pd.concat([Haupt_df, col_df])
+                haupt_df = pd.concat([haupt_df, col_df])
 
-            final_df = Haupt_df[desired_output]
+            final_df = haupt_df[desired_output]
             final_df.to_excel(os.path.join(self.data_dir), 'env_data_' + self.freq + 'txt')
             return final_df
 
 
-def assign_freq(df, index=None, file=None, force_freq=None,  verbosity=1, print_only = False):
+def assign_freq(df, index=None, file=None, force_freq=None,  verbosity=1, print_only=False):
     if not print_only:
         if index is None:
             idx = pd.to_datetime(df['Date_Time'])
@@ -241,7 +249,8 @@ def assign_freq(df, index=None, file=None, force_freq=None,  verbosity=1, print_
     if df.index.freq is None:
         if force_freq is not None:
             df.index.freq = force_freq
-    if verbosity > 0: print('in file {} frequency is {}'.format(file, df.index.freq))
+    if verbosity > 0:
+        print('in file {} frequency is {}'.format(file, df.index.freq))
     return df
 
 
@@ -253,7 +262,8 @@ def down_sample(data_frame, data_name, desired_freq, idx,  verbosity=1):
     elif 'Date_Time' in data_frame.columns:
         data_frame.index = data_frame['Date_Time']
 
-    if verbosity>1: print(data_frame.shape, data_frame.columns)
+    if verbosity > 1:
+        print(data_frame.shape, data_frame.columns)
 
     if not isinstance(data_frame.index, pd.DatetimeIndex):
         raise TypeError("index of data_frame must be of Datetime")
@@ -261,7 +271,8 @@ def down_sample(data_frame, data_name, desired_freq, idx,  verbosity=1):
     out_freq = desired_freq
     data_frame = data_frame.copy()
     old_freq = data_frame.index.freq
-    if verbosity > 0: print('downsampling {} data from {} min to {}'.format(data_name, old_freq, out_freq))
+    if verbosity > 0:
+        print('downsampling {} data from {} min to {}'.format(data_name, old_freq, out_freq))
     # e.g. from hourly to daily
     if data_name in ['air_temp_c', 'rel_hum', 'wind_dir_deg', 'wind_speed_mps', 'air_p_hpa', 'mslp_hpa',
                      'tide_cm', 'wat_temp_c', 'sal_psu']:
@@ -269,9 +280,9 @@ def down_sample(data_frame, data_name, desired_freq, idx,  verbosity=1):
     elif data_name in ['pcp_mm', 'ss_gpl', 'solar_rad']:
         return data_frame.resample(out_freq).sum()
     else:
-        if verbosity>0: print('not resampling ', data_name)
+        if verbosity > 0:
+            print('not resampling ', data_name)
         return data_frame
-
 
 
 def load_1min_gwangali_sitewise():
@@ -280,7 +291,7 @@ def load_1min_gwangali_sitewise():
     _files = [f for f in os.listdir(_d_dir) if f.endswith('txt')]
     a_files, b_files = [], []
     for f in _files:
-        if f.split('.')[0].endswith('_a'):
+        if f.split('.')[0].endswith("_a"):
             a_files.append(f)
         elif f.split('.')[0].endswith('_b'):
             b_files.append(f)
@@ -304,7 +315,7 @@ def load_1min_gwangali_sitewise():
 
 
 # function to remove a chunk of rows from dataframe
-def remove_chunk(_st,_en,_df):
+def remove_chunk(_st, _en, _df):
     st_indx = _df.index[0]
     en_indx = _df.index[-1]
     dfs = _df[st_indx: _st]
@@ -315,4 +326,4 @@ def remove_chunk(_st,_en,_df):
 
 # if __name__ == "__main__":
 #     data = DATA()
-    #data.plot_data()
+    # data.plot_data()
