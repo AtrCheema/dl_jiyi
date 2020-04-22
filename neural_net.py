8eabfcdfffb2fe0497b1552a07c23b3b2f97e806
AtrCheema <ather_abbas786@yahoo.com>
2020-04-22 10:08:12 +0900

    pep8 compatible

--- neural_net.py
+++ neural_net.py
@@ -7,7 +7,7 @@ import tensorflow.keras.backend as K
 from tensorflow.python.ops.rnn import dynamic_rnn
 from TSErrors import FindErrors
 
-#from utils import check_min_loss, batch_generator, Batch_Generator
+# from utils import check_min_loss, batch_generator, Batch_Generator
 from leaky_dense import LeakyDense2D
 
 import numpy as np
@@ -37,7 +37,6 @@ class Model(object):
         self.config = {}
         self.verbose = verbose
 
-
     def build_nn(self):
 
         reset_graph()
@@ -63,18 +62,21 @@ class Model(object):
         else:
             rnn_layer = RNN(cell)
             rnn_outputs = rnn_layer(self.x_ph)  # [batch_size, neurons]
-            if self.verbose > 0: print(rnn_outputs.shape, 'before reshaping', K.eval(tf.rank(rnn_outputs)))
+            if self.verbose > 0:
+                print(rnn_outputs.shape, 'before reshaping', K.eval(tf.rank(rnn_outputs)))
             rnn_outputs = tf.reshape(rnn_outputs[:, :], [-1, self.nn_conf['lstm_units']])
-            if self.verbose > 0: print(rnn_outputs.shape, 'after reshaping', K.eval(tf.rank(rnn_outputs)))
+            if self.verbose > 0:
+                print(rnn_outputs.shape, 'after reshaping', K.eval(tf.rank(rnn_outputs)))
 
         if self.nn_conf['batch_norm']:
             rnn_outputs3 = BatchNormalization()(rnn_outputs)
-            rnn_outputs = Activation('relu')(rnn_outputs)
+            rnn_outputs = Activation('relu')(rnn_outputs3)
 
         if self.nn_conf['dropout'] is not None:
             rnn_outputs = Dropout(self.nn_conf['dropout'])(rnn_outputs)
 
-        if self.verbose > 0: print(rnn_outputs.shape, "dynamic_rnn/RNN outputs shape", rnn_outputs.get_shape())
+        if self.verbose > 0:
+            print(rnn_outputs.shape, "dynamic_rnn/RNN outputs shape", rnn_outputs.get_shape())
 
         leaky_layer = LeakyDense2D(units=self.nn_conf['output_features'], activation=tf.nn.elu, leaky_inputs=True,
                                    mask_array=self.mask_ph, verbose=self.verbose)  # tf.nn.elu
@@ -85,7 +87,8 @@ class Model(object):
         outputs = tf.reshape(dense_outputs, [-1, 1])  # this is because observations will also be flattened
         self.full_outputs = leaky_layer.full_outputs
 
-        if self.verbose > 0: print(outputs.shape, 'shape outputs')
+        if self.verbose > 0:
+            print(outputs.shape, 'shape outputs')
 
         self.loss = tf.reduce_mean(tf.square(outputs - self.obs_y_ph))
         optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=self.nn_conf['lr'])
